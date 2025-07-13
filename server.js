019f168959043dd2c4b23a3824c2416c74f1cf62
hadi osama <hadiosama622@gmail.com>
2025-07-13 16:02:35 +0300

    bulid landing page

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+
+// MIDDLEWARE
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(3000, () => {
     console.log('Listenig on port 3000')
 })
 
 app.get('/', (req, res)=>{
-res.send('hello, friend')
+res.render('index.ejs')
 })
