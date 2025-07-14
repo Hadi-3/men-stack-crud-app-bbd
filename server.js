a062acd70115364ca615d01a8491e5e8413e3cd4
hadi osama <hadiosama622@gmail.com>
2025-07-14 09:25:48 +0300

    Connect to database

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const path = require('path')
+
+// DATABASE CONNECTION
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected', () =>{
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`)
+})
 
 // MIDDLEWARE
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000, () => {
-    console.log('Listenig on port 3000')
-})
-
+// GET / (home)
 app.get('/', (req, res)=>{
 res.render('index.ejs')
+})
+
+app.listen(3000, () => {
+    console.log('Listenig on port 3000')
 })
